4789903a16156d6e587996df37932f1db6ddeeef
unknown <yefrycutzalsimilox@gmail.com>
2023-04-02 12:03:23 -0600

    se integro la vista de envio de peticiones en el rule usuario

--- hms/include/sidebar.php
+++ hms/include/sidebar.php
@@ -57,6 +57,18 @@
 									</div>
 								</a>
 							</li>
+							<li>
+								<a href="peticion.php">
+									<div class="item-content">
+										<div class="item-media">
+											<i class="ti-list"></i>
+										</div>
+										<div class="item-inner">
+											<span class="title"> Peticion de Oracion </span>
+										</div>
+									</div>
+								</a>
+							</li>
 
 						</ul>
 						<!-- end: CORE FEATURES -->
